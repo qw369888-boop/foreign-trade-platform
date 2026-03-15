ff34ad5a6943ccb8006374ffbef371ff2bf09eee
qw369888-boop <qw369888@gmail.com>
2026-03-16 02:41:38 +0800

    Add fixed static website files for GitHub Pages deployment

---
+++ frontend/out/_next/static/mSBscAxqDFGt92qTPNfe9/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fcart","\u002Fcheckout","\u002Fcontact","\u002Flogin","\u002Forder-success","\u002Fproducts","\u002Fproducts\u002F[id]","\u002Fregister"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
